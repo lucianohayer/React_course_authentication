508f2aacbf6550ceac91a653cb438a79c8abc81d
Luciano Hayer <lucianohayer@gmail.com>
2021-09-12 02:58:31 +0200

    Bcrypt and jwt implementation

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,12 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+const { token } = require('morgan');
+
+function tokenForUser(user) {
+	const timestamp = new Date().getTime();
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function (req, res, next) {
 	const email = req.body.email;
@@ -28,7 +36,7 @@ exports.signup = function (req, res, next) {
 			if (err) {
 				return next(err);
 			}
-			res.json({ success: true });
+			res.json({ token: tokenForUser(user) });
 		});
 	});
 };
