d2e83752d48705104a80debdce6fd09ea0e3da87
Luciano Hayer <lucianohayer@gmail.com>
2021-09-15 00:54:55 +0200

    create signup actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,7 @@
+import { AUTH_USER } from './types';
+
+import axios from 'axios';
+
+export const signup = (formProps) => (dispatch) => {
+	axios.post('http://localhost:3090/signup', formProps);
+};
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { signup } from '../../actions';
 
 class Signup extends Component {
+	onSubmit = (formProps) => {
+		this.props.signup(formProps);
+	};
+
 	render() {
+		const { handleSubmit } = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit)}>
 				<fieldset>
 					<label>Email</label>
+					<Field
+						name="email"
+						type="text"
+						component="input"
+						autoComplete="none"
+					/>
 				</fieldset>
 				<fieldset>
 					<label>Password</label>
+					<Field
+						name="password"
+						type="password"
+						component="input"
+						autoComplete="none"
+					/>
 				</fieldset>
+				<button>Sign Up!</button>
 			</form>
 		);
 	}
 }
 
-export default Signup;
+export default compose(
+	connect(null, { signup }),
+	reduxForm({ form: 'signup' })
+)(Signup);
